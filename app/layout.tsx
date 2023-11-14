f98e21206690214488f4a512b1873d258ccff253
KacVu4-8 <khacvu908@gmail.com>
2023-11-14 10:14:22 +0700

    Navbar UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Nunito } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Navbar from "./components/navbar/Navbar";
 
 export const metadata: Metadata = {
   title: "Aribnb",
@@ -19,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
