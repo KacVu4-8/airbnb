9dc2c6b730bdabacf961cc8322a76d3aad21824e
KacVu4-8 <khacvu908@gmail.com>
2023-11-28 08:55:18 +0700

    Counter Components

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -48,10 +48,17 @@ const RentModal = () => {
 
   const category = watch("category");
   const location = watch("location");
+  const guestCount = watch("guestCount");
+  const roomCount = watch("roomCount");
+  const bathroomCount = watch("bathroomCount");
 
-  const Map = useMemo(() => dynamic(() => import('../Map'), {
-    ssr: false,
-  }), [location]);
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -128,20 +135,28 @@ const RentModal = () => {
           subtitle="What amenities do you have?"
         />
         <Counter
-          // onChange={(value) => setCustomValue("guestCount", value)}
-          // value={guestCount}
-          // title="Guests"
-          // subtitle="How many guests do you allow?"
+          title="Guests"
+          subtitle="How many guests do you allow?"
+          value={guestCount}
+          onChange={(value) => setCustomValue("guestCount", value)}
         />
         <hr />
         <Counter
-          // onChange={(value) => setCustomValue("roomCount", value)}
-          // value={roomCount}
-          // title="Rooms"
-          // subtitle="How many rooms do you have?"
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+          value={roomCount}
+          onChange={(value) => setCustomValue("roomCount", value)}
         />
-        </div>
-        )}
+        <hr />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+          value={bathroomCount}
+          onChange={(value) => setCustomValue("bathroomCount", value)}
+        />
+      </div>
+    );
+  }
 
   return (
     <Modal
