2d22c0fb267940b7bfb8a89d5991f7c78da87a67
KacVu4-8 <khacvu908@gmail.com>
2023-11-29 15:19:35 +0700

    Listing card components, direct sever action

--- app/components/inputs/CategoryInput.tsx
+++ app/components/inputs/CategoryInput.tsx
@@ -1,15 +1,49 @@
+// "use client";
+
+// import { IconType } from "react-icons";
+
+// interface CategoryInputProps {
+//   icon: IconType;
+//   label: string;
+//   selected?: boolean;
+//   onClick: (value: string) => void;
+// }
+
+// const CategoryInput: React.FC<CategoryInputProps> = ({
+//   icon: Icon,
+//   label,
+//   selected,
+//   onClick,
+// }) => {
+//   return (
+//     <div
+//       onClick={() => onClick(label)}
+//       className={`rounded-xl border-2 p-4 flex flex-col gap-3 hover:border-black transition cursor-pointer
+//       ${selected ? "border-black" : "border-neutral-200"}
+//       `}
+//     >
+//         <Icon size={30}/>
+//         <div className="font-semibold">
+//             {label}
+//         </div>
+//     </div>
+//   );
+// };
+
+// export default CategoryInput;
+
 "use client";
 
 import { IconType } from "react-icons";
 
-interface CategoryInputProps {
+interface CategoryBoxProps {
   icon: IconType;
   label: string;
   selected?: boolean;
   onClick: (value: string) => void;
 }
 
-const CategoryInput: React.FC<CategoryInputProps> = ({
+const CategoryBox: React.FC<CategoryBoxProps> = ({
   icon: Icon,
   label,
   selected,
@@ -18,16 +52,23 @@ const CategoryInput: React.FC<CategoryInputProps> = ({
   return (
     <div
       onClick={() => onClick(label)}
-      className={`rounded-xl border-2 p-4 flex flex-col gap-3 hover:border-black transition cursor-pointer
-      ${selected ? "border-black" : "border-neutral-200"}
+      className={`
+        rounded-xl
+        border-2
+        p-4
+        flex
+        flex-col
+        gap-3
+        hover:border-black
+        transition
+        cursor-pointer
+        ${selected ? "border-black" : "border-neutral-200"}
       `}
     >
-        <Icon size={30}/>
-        <div className="font-semibold">
-            {label}
-        </div>
+      <Icon size={30} />
+      <div className="font-semibold">{label}</div>
     </div>
   );
 };
 
-export default CategoryInput;
+export default CategoryBox;
